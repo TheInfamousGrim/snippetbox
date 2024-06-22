7de4950267d7af17f73dee530a1b099e9812794c
TheInfamousGrim <george@grimfunky.dev>
2024-06-22 21:05:19 +0100

    ✨ Complete serving static files exc

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,8 +7,20 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	//* File Server
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	//* Static Assets
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	//* Home
 	mux.HandleFunc("GET /{$}", home)
+
+	//* Snippet View
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+
+	//* Snippet Create
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
